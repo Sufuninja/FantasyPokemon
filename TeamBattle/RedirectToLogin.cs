39c2e009632395f5b899541972188720ae342987
William Campbell <sufuninja@gmail.com>
2021-05-21 00:29:51 -0500

    Redirect to login if not logged in

---
+++ TeamBattle/RedirectToLogin.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TeamBattle
+{
+    public class RedirectToLogin : ComponentBase
+    {
+        [Inject] protected NavigationManager NavigationManager { get; set; }
+
+        protected override void OnInitialized()
+        {
+            NavigationManager.NavigateTo("identity/account/login");
+        }
+    }
+}
